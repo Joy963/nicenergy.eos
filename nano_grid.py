6132dfcf6162c1586173e2451fc18129a2f96ba6
Joy963 <yzg75001@gmail.com>
2017-08-02 11:00:07 +0800

    add upload method

--- nano_grid.py
+++ nano_grid.py
@@ -222,7 +222,6 @@ def recv_device_data():
                 }, data.get('runData', {}).items()))
 
                 token = device_token_map.get(dev_id.upper(), {}).get('token', '')
-                logger.info("%s: %s", dev_id, token)
                 r = requests.post(DATA_UPLOAD_API, json={'token': token, 'data': data_list}).content
                 logger.info(r)
             if flag & select.POLLOUT:
