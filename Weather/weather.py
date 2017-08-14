630fe4fe9318e788546073243c811a8a416bbdee
Joy963 <yzg75001@gmail.com>
2017-08-14 13:32:11 +0800

    crontab weather

--- Weather/weather.py
+++ Weather/weather.py
@@ -59,11 +59,16 @@ def save_weather_data(d):
 if __name__ == '__main__':
     now = datetime.datetime.now()
     timestamp = int(time.mktime(datetime.datetime(now.year, now.month, now.day).timetuple()))
-    for _ in range(90):
-        new_timestamp = timestamp-_*86400
-        _data = get_weather_data(ts=new_timestamp)
-        _data['data'] = str(datetime.datetime.fromtimestamp(new_timestamp))
-        logger.info(datetime.datetime.fromtimestamp(new_timestamp))
-        save_weather_data(_data)
+    # for _ in range(90):
+    #     new_timestamp = timestamp-_*86400
+    #     _data = get_weather_data(ts=new_timestamp)
+    #     _data['data'] = str(datetime.datetime.fromtimestamp(new_timestamp))
+    #     logger.info(datetime.datetime.fromtimestamp(new_timestamp))
+    #     save_weather_data(_data)
+
+    _data = get_weather_data(ts=timestamp)
+    _data['data'] = str(datetime.datetime.fromtimestamp(timestamp))
+    logger.info(datetime.datetime.fromtimestamp(timestamp))
+    save_weather_data(_data)
 
 
