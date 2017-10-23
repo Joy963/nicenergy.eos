7defc465d9504792e00595599f861e4aaabf18d8
Joy963 <yzg75001@gmail.com>
2017-10-23 14:25:21 +0800

    rm debug info

--- nano_grid.py
+++ nano_grid.py
@@ -291,8 +291,8 @@ def upload_data_to_cloud(task_id=-1):
 
         if log_count[dev_id] >= 60:
             log_count[dev_id] = 0
-            logger.info('[%s:%d] dev_id: %-12s send_len: %d response: %s',
-                        address, port, dev_id, len(data_list), r)
+            logger.info('[task_id: %d][%s:%d] dev_id: %-12s send_len: %d response: %s',
+                        task_id, address, port, dev_id, len(data_list), r)
         log_count[dev_id] += 1
 
 
