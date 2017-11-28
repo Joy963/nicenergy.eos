9f6c24f9908c1d101ef6ce496c53ab7bd9eb3b89
Joy963 <yzg75001@gmail.com>
2017-11-28 15:53:00 +0800

    switch cmd

--- nano_grid.py
+++ nano_grid.py
@@ -153,6 +153,7 @@ def cmd_server():
                         try:
                             d = json.loads(data.decode('utf-8'))
                             if d.get('cmd') in ['﻿DistSwitch1Open', '﻿DistSwitch1Close']:
+                                logger.info(d.get('cmd'))
                                 d['device_id'] = 'power_distribution_simulation'
                             logger.info(d)
                             if all([d.get('device_id'), d.get('cmd')]):
